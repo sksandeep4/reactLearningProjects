29ed654615ccdbd6cb0fb60fa0ba06008d81931b
Sandeep <sandeepeos04@gmail.com>
2024-07-02 01:52:54 +0530

    Just another commit 2

--- StepsToProgrammer/src/App.js
+++ StepsToProgrammer/src/App.js
@@ -1,3 +1,31 @@
-export default App = () => {
-  return <div>Hello React!</div>;
+const App = () => {
+  const messages = [
+    "Learn React ⚛️",
+    "Build Projects 🛠",
+    "Apply for jobs 💼",
+    "Invest your new income 🤑",
+  ];
+  const step = 1;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
+
+export default App;
